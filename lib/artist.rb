b66ad8508cf455f08bec2f7bf9a591df1e16dc63
Daniel Sasser <danielbsasser@gmail.com>
2020-02-18 22:47:11 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    s = Song.new(name)
+    self.add_song(s)
+    s
+  end
+end
