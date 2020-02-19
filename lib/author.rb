e27068e7e5b1a21e5cc88ab426b981e652f022b3
Daniel Sasser <danielbsasser@gmail.com>
2020-02-19 02:28:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -21,7 +21,7 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
-    #self.add_song(song)
+    self.add_song(song)
     post.author = self
     @@post_count_total += 1
   end
