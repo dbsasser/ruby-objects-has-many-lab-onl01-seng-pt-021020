3257e3c153223c88d3033ff27bd42896c7018d48
Daniel Sasser <danielbsasser@gmail.com>
2020-02-19 02:22:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author 
   
   attr_accessor :name 
+  @@post_count_total = 0 
   
   def initialize(name)
     @name = name
@@ -9,4 +10,13 @@ class Author
   def posts 
     Post.all.select {|post| post.author == self}
   end
+  
+  def add_post
+      
+  end
+  
+  def add_post_by_title
+    
+  end
+  
 end
